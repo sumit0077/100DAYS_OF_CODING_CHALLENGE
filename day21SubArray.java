c38a96bda4036288054a7891b3a9b7210c1c8d61
Sumit kumar <53577148+sumit0077@users.noreply.github.com>
2022-11-12 23:36:37 +0530

    Update and rename day21.java to day21SubArray.java

--- day21SubArray.java
+++ day21SubArray.java
@@ -4,7 +4,8 @@ import java.util.*;
 public class Solution {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */int n,sum=0,count=0;
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        int n,sum=0,count=0;
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
         int[] arr = new int[n];
