472f956cd05d0e60d074cf008ef9912f75aff210
Sumit kumar <53577148+sumit0077@users.noreply.github.com>
2022-11-12 23:34:51 +0530

    Day21 JAVA_SubArray

---
+++ day21.java
@@ -0,0 +1,34 @@
+mport java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */int n,sum=0,count=0;
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<arr.length;i++){
+            arr[i] = sc.nextInt();
+        }
+        for(int i=0;i<arr.length;i++)
+        {
+            for(int j=i;j<arr.length;j++)
+            {
+                sum = 0;
+                for(int k=i;k<=j;k++)
+                {
+                    sum+=arr[k];
+                   
+                }
+                 if(sum<0)
+                 {
+                     count++;
+                 }
+            }
+        }
+        System.out.print(count);
+    }
+    
+}
+
