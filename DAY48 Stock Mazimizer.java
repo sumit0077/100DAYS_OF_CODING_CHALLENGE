ac402200480a75e26824cc86ddd76c4913cbe5f4
Sumit kumar <53577148+sumit0077@users.noreply.github.com>
2022-12-11 12:12:26 +0530

    Create DAY48 Stock Mazimizer.java
    Sample Input
    
    STDIN       Function
    -----       --------
    3           q = 3
    3           prices[] size n = 3
    5 3 2       prices = [5, 3, 2]
    3           prices[] size n = 3
    1 2 100     prices = [1, 2, 100]
    4           prices[] size n = 4
    1 3 1 2     prices =[1, 3, 1, 2]
    
    Sample Output
    
    0
    197
    3

---
+++ DAY48 Stock Mazimizer.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'stockmax' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts INTEGER_ARRAY prices as parameter.
+     */
+
+    public static long stockmax(List<Integer> prices) {
+    // Write your code here
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                List<Integer> prices = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                    .map(Integer::parseInt)
+                    .collect(toList());
+
+                long result = Result.stockmax(prices);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
