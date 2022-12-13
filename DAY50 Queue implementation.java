af7a61f64e3cc0f86a9024af105202655e79c0ad
Sumit kumar <53577148+sumit0077@users.noreply.github.com>
2022-12-13 18:48:11 +0530

    Create DAY50 Queue implementation.java

---
+++ DAY50 Queue implementation.java
@@ -0,0 +1,36 @@
+class Queue {
+
+    Node head;
+    Node tail;
+
+    void add(Node n) {
+        if (tail != null) {
+            tail.next = n;
+        }
+        tail = n;
+        if (head == null) {
+            head = n;
+        }
+    }
+
+    Node remove() {
+        if (head == null) {
+            throw new EmptyStackException();
+        }
+        Node n = head;
+        head = head.next;
+
+        if (head == null) {
+            tail = null;
+        }
+        return n;
+    }
+
+    Node peek() {
+        return head;
+    }
+
+    boolean isEmpty() {
+        return tail == null;
+    }
+}
