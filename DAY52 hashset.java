120177ec4fc04315b3eec8263593932d0fc3611e
Sumit kumar <53577148+sumit0077@users.noreply.github.com>
2022-12-16 23:31:26 +0530

    Create DAY52 hashset.java
    Sample Input
    
    5
    john tom
    john mary
    john tom
    mary anna
    mary anna
    
    Sample Output
    
    1
    2
    2
    3
    3
    
    nput (stdin)
    
        5
    
        john tom
    
        john mary
    
        john tom
    
        mary anna
    
        mary anna

---
+++ DAY52 hashset.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution 
+{
+
+    public static void main(String[] args) {
+
+        HashSet  hs = new HashSet ();
+        int count = 0;
+        Scanner sc = new Scanner(System.in);
+        int numCases = Integer.parseInt(sc.nextLine());
+        for(int i = 0;i < numCases;i++){
+            if(hs.add(sc.nextLine())){
+                count++;
+            }
+            System.out.println(count);
+        }
+
+    }
+}
