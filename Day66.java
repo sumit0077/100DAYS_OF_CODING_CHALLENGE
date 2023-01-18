7f2d7f68d69928199e1cb3613ea7c067cdcb5766
Sumit kumar <53577148+sumit0077@users.noreply.github.com>
2023-01-18 22:16:51 +0530

    Update Day66.java
    Example 1:
    
    Input: nums = [1,3,5,6], target = 5
    Output: 2
    
    Example 2:
    
    Input: nums = [1,3,5,6], target = 2
    Output: 1
    
    Example 3:
    
    Input: nums = [1,3,5,6], target = 7
    Output: 4

--- Day66.java
+++ Day66.java
@@ -1,1 +1,27 @@
+Q- leetcode(35)
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+You must write an algorithm with O(log n) runtime complexity.
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+      //int j=0;
+      int lo=0;
+      int hi=nums.length-1;
+      while (lo<=hi){
+      int mid= lo +(hi-lo)/2;
+      if(nums[mid]<target)
+     {
+         lo=mid+1;
+     }
+     else if(nums[mid]>target)
+     {
+         hi=mid-1;
+     }
+     else
+     {
+         return mid;
+     }
 
+    }
+    return lo;
+}
+}
