afbc5a31e1e32f9bfd6b64a3d0cc82591bf4a5fd
Sumit kumar <53577148+sumit0077@users.noreply.github.com>
2023-01-19 23:56:15 +0530

    Update Day67.java

--- Day67.java
+++ Day67.java
@@ -4,10 +4,6 @@ Given a string s consisting of words and spaces, return the length of the last w
 A word is a maximal
 substring
 consisting of non-space characters only.
-
-
-
-
 class Solution {
     public int lengthOfLastWord(String s) {
         char[] arr=s.toCharArray();
