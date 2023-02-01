e927d30231c03c570973fa46b8cff812016eb2de
Sumit kumar <53577148+sumit0077@users.noreply.github.com>
2023-02-01 22:32:56 +0530

    Update Day77.java

--- Day77.java
+++ Day77.java
@@ -1,1 +1,39 @@
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
 
+Note that you must do this in-place without making a copy of the array.
+
+ 
+
+Example 1:
+
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+
+Example 2:
+
+Input: nums = [0]
+Output: [0]
+
+ 
+class Solution {
+   static void swap(int[] nums, int i, int j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
+
+    public void moveZeroes(int[] nums) {
+        int n=nums.length;
+        for(int i=0; i<n; i++){
+            if(nums[i]!=0){
+                continue;
+            }else{
+                int j=i;
+                while(j<n-1 && nums[j]==0){
+                    j++;
+                }
+                swap(nums, i, j);
+            }
+        }
+    }
+}
