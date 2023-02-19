e18e906894508e3eeed0409f4183d5d4b206dfc1
Sumit kumar <53577148+sumit0077@users.noreply.github.com>
2023-02-19 23:59:46 +0530

    Update day84.java

--- day84.java
+++ day84.java
@@ -1,1 +1,26 @@
-
+4SUM. leetcode
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        Set<List<Integer>> set = new HashSet<>();
+        for(int i = 0; i < nums.length - 3; i++) {
+            for(int j = i + 1; j < nums.length - 2; j++) {
+                int k = j + 1;
+                int l = nums.length - 1;
+                while(k < l) {
+                    long sum = (long)nums[i] + (long)nums[j] + (long)nums[k] + (long)nums[l];
+                    if(sum == target){
+                        set.add(List.of(nums[i],nums[j],nums[k],nums[l]));
+                        k++;
+                        l--;
+                    }
+                    else if(sum < target)
+                        k++;
+                    else
+                        l--;
+                }
+            }
+        }
+        return new ArrayList<>(set);
+    }
+}
