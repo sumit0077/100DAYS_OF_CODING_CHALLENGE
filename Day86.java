d552abb24455ba3fb39372f78e324fdc36b039b8
Sumit kumar <53577148+sumit0077@users.noreply.github.com>
2023-02-22 20:39:11 +0530

    Update Day86.java
    Example 1:
    
    Input: s = "egg", t = "add"
    Output: true
    
    Example 2:
    
    Input: s = "foo", t = "bar"
    Output: false
    
    Example 3:
    
    Input: s = "paper", t = "title"
    Output: true

--- Day86.java
+++ Day86.java
@@ -1,1 +1,25 @@
-ss
+Given two strings s and t, determine if they are isomorphic.
+Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+All occurrences of a character must be replaced with another character while preserving the order of characters. 
+No two characters may map to the same character, but a character may map to itself.
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+
+        int map1[]=new int[200];
+        int map2[]=new int[200];
+
+        if(s.length()!=t.length())
+            return false;
+
+
+        for(int i=0;i<s.length();i++)
+        {
+            if(map1[s.charAt(i)]!=map2[t.charAt(i)])
+                return false;
+
+            map1[s.charAt(i)]=i+1;
+            map2[t.charAt(i)]=i+1;
+        }
+        return true;
+    }
+}
