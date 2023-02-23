7ccefc5be63468ea45cdd38c26f8d11a1eea08bb
Sumit kumar <53577148+sumit0077@users.noreply.github.com>
2023-02-23 21:59:38 +0530

    Update Day87.java
    Example 1:
    
    Input: nums = [2,2,1]
    Output: 1
    
    Example 2:
    
    Input: nums = [4,1,2,1,2]
    Output: 4
    
    Example 3:
    
    Input: nums = [1]
    Output: 1

--- Day87.java
+++ Day87.java
@@ -1,1 +1,15 @@
-jkhh
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) { 
+       unordered_map<int,int> a;
+	   for(auto x: nums)
+		   a[x]++;
+	   for(auto z:a)
+		   if(z.second==1)
+			   return z.first;
+	   return -1;
+    }
+};
